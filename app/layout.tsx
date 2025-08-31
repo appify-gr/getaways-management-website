712378b16d54ad8c963fcc518dc23aa0e171106b
Vaggelis Magonezos <nospipian@gmail.com>
2025-08-31 10:42:46 +0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <body className={roboto.className}>{children}</body>
     </html>
   );
